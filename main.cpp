7b9287e99e23df6fd7a4d370f96142ddf88d3c14
DarkDeuse <sasha12345678_92@mail.ru>
2024-06-04 02:21:11 +0300

    первый залив кода

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "matrix.h"
+#include <iostream>
+#include <cassert>
+
+int main(int, char **) {
+    Matrix<int, -1> matrix;
+    assert(matrix.size() == 0);
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+
+    Matrix<int, 0> m0;
+    auto size = 10;
+    auto ij = 0;
+    auto ji = 9;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (i == j) m0[i][j] = ij++;
+            if (i + j == size - 1) m0[i][j] = ji--;
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            std::cout << m0[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << m0.size();
+
+    return 0;
+}
